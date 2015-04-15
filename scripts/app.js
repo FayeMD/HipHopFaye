538f80619876b467b6a556dd09bcda2db7787115
FayeMD <fayemcdonald@hotmail.com>
2015-04-15 10:16:38 -0300

    Task 2 Adding notification with Toaster

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,8 @@ var app = angular
     'ngAnimate',
     'ngResource',    
     'ngRoute',    
-    'firebase'
+    'firebase',
+    'toaster'
   ])
   .constant('FURL', 'https://fayemd.firebaseio.com/')  
   .config(function ($routeProvider) {
