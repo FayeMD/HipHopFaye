fad964a94b2424b373932448485220b2708df8c6
FayeMD <fayemcdonald@hotmail.com>
2015-04-16 16:29:20 -0300

    back to my original auth.js - controllers

--- scripts/controllers/auth.js
+++ scripts/controllers/auth.js
@@ -1,65 +1,49 @@
-app.controller('AuthController', function($scope, $location, toaster, Auth) {
+'user strict';
 
-  if(Auth.signedIn()) {
-    $location.path('/');
-  }
+app.controller('AuthController', function($scope, $location, Auth,) {
 
-	$scope.register = function(user) {          
-    Auth.register(user)
-      .then(function() {
-        toaster.pop('success', "Registered successfully");
-        $location.path('/dashboard');
-      }, function(err) {
-        errMessage(err);
-      });
-  };
+	if(Auth.signedIn()) {
+		$location.path('/');
+	}
+
+	$scope.register = function(user) {
+		Auth.register(user).then(function() {
+			toaster.pop('success', 'Registered successfully!');
+			//console.log("Register successfully!");
+			$location.path('/');
+		}, function(err) {
+			toaster.pop('error', 'Oops, something went wrong!');
+			//console.log("Error...");
+		});
+	};
 
 	$scope.login = function(user) {
-     Auth.login(user)
-      .then(function() {
-        toaster.pop('success', "Logged in successfully");
-        $location.path('/dashboard');
-      }, function(err) {        
-        errMessage(err);
-      });    
+
+		Auth.login(user)
+		.then(function() {
+			toaster.pop('success', 'Logged in successfully!');
+			//console.log("Logged in successfully!");
+			$location.path('/');
+		}, function(err) {
+			toaster.pop('error', 'Oops, something went wrong!');
+			//console.log("Error...");
+		});
 	};
 
 	$scope.changePassword = function(user) {
-     Auth.changePassword(email, oldPass, newPass)
-      .then(function() {                        
-        
-        // Reset form
-        $scope.email = '';
-        $scope.oldPass = '';
-        $scope.newPass = '';
 
-        toaster.pop('success', "Password changed successfully");
-      }, function(err) {
-        errMessage(err);      
-      });
-  };
+		Auth.changePassword(user)
+		.then(function() {
 
-	function errMessage(err) {
+			//Reset form
+			$scope.user.email = '';
+			$scope.user.oldPass = '';
+			$scope.user.newPass = '';
 
-    var msg = "Unknown Error...";
-
-    if(err && err.code) {
-      switch (err.code) {
-        case "EMAIL_TAKEN": 
-          msg = "This email has been taken"; break;          
-        case "INVALID_EMAIL": 
-          msg = "Invalid email"; break;          
-        case "NETWORK_ERROR": 
-          msg = "Network error"; break;          
-        case "INVALID_PASSWORD": 
-          msg = "Invalid password"; break;          
-        case "INVALID_USER":
-          msg = "Invalid user"; break;                  
-      } 
-    }   
-
-    toaster.pop('error', msg);
-  };
-
+			console.log("Password changed successfully!");
+		}, function(err) {
+			console.log("Error...");
+		});
+	};
 
 });
