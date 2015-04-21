f9e6d1748b7f967464ba384b9e235435bffb69ee
FayeMD <fayemcdonald@hotmail.com>
2015-04-21 14:04:09 -0300

    Task 4

--- scripts/controllers/browse.js
+++ scripts/controllers/browse.js
@@ -1,10 +1,11 @@
 'use strict';
 
-app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth) {
+app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth, Comment) {
 
 	$scope.searchTask = '';		
 	$scope.tasks = Task.all;
 
+	$scope.user = Auth.user;
 	$scope.signedIn = Auth.signedIn;
 
 	$scope.listMode = true;
@@ -18,22 +19,30 @@ app.controller('BrowseController', function($scope, $routeParams, toaster, Task,
 	function setSelectedTask(task) {
 		$scope.selectedTask = task;
 		
-		// We check isTaskCreator only if user signedIn 
-		// so we don't have to check every time normal guests open the task
 		if($scope.signedIn()) {
-			// Check if the current login user is the creator of selected task
 			$scope.isTaskCreator = Task.isCreator;
-			
-			// Check if the selectedTask is open
 			$scope.isOpen = Task.isOpen;			
 		}
-	};
 
-	// --------------- TASK ---------------	
+		$scope.comments = Comment.comments(task.$id);
+	};
 
 	$scope.cancelTask = function(taskId) {
 		Task.cancelTask(taskId).then(function() {
 			toaster.pop('success', "This task is cancelled successfully.");
 		});
 	};
+
+	$scope.addComment = function() {
+		var comment = {
+			content: $scope.content,
+			name: $scope.user.profile.name,
+			gravatar: $scope.user.profile.gravatar
+		};
+
+		Comment.addComment($scope.selectedTask.$id, comment).then(function() {
+			$scope.content = '';
+		});
+	};
+
 });
